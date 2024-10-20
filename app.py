846a57fe9223b5240eac9c2b90c187ee9f17229c
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-10-20 18:53:57 +0300

    Created migration object and initialized flask app

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from models import *
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dating_site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
