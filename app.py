dea45517dcaddc69bf53a73aac28e5f7612fbddd
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-10-20 18:47:51 +0300

    added configurations for database connection and disable modification tracking

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dating_site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
